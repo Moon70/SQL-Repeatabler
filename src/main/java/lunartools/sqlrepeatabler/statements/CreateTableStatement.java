3cec473217507d945e91fd5bad351358e49cf0a6
Moon70 <52245419+Moon70@users.noreply.github.com>
2025-11-15 19:41:45 +0100

    refactoring, indent changed from tab to space

--- src/main/java/lunartools/sqlrepeatabler/statements/CreateTableStatement.java
+++ src/main/java/lunartools/sqlrepeatabler/statements/CreateTableStatement.java
@@ -25,21 +25,21 @@ public class CreateTableStatement implements Statement{
 	public void toSqlCharacters(SqlBlock sqlBlock) throws Exception {
 		SqlBlock sqlBlockStatement=new SqlBlock();
 		
-		sqlBlockStatement.add(SqlString.createSqlStringFromString("IF OBJECT_ID(N'%s', 'U') IS NULL", Category.INSERTED, tableName.getFullNameWithoutDelimiter()));
-		sqlBlockStatement.add(SqlString.createSqlStringFromString("BEGIN", Category.INSERTED));
-		sqlBlockStatement.add(SqlString.createSqlStringFromString("\t%s %s (",Category.INSERTED,tokenStatement,tableName.getFullName()));
+		sqlBlockStatement.add(SqlString.createSqlStringFromString(	"IF OBJECT_ID(N'%s', 'U') IS NULL"	,Category.INSERTED, tableName.getFullNameWithoutDelimiter()));
+		sqlBlockStatement.add(SqlString.createSqlStringFromString(	"BEGIN"								,Category.INSERTED));
+		sqlBlockStatement.add(SqlString.createSqlStringFromString(	"    %s %s ("						,Category.INSERTED,tokenStatement,tableName.getFullName()));
 
 		SqlString sqlString;
 		for(int i=0;i<tokens.size();i++) {
-			sqlString=SqlString.createSqlStringFromString("\t\t%s", Category.INSERTED,tokens.get(i));
+			sqlString=SqlString.createSqlStringFromString(			"        %s"						,Category.INSERTED,tokens.get(i));
 			if(i<tokens.size()-1) {
 				sqlString.append(new SqlCharacter(',',Category.INSERTED));
 			}
 			sqlBlockStatement.add(sqlString);
 		}
 
-		sqlBlockStatement.add(SqlString.createSqlStringFromString("\t);", Category.INSERTED));
-		sqlBlockStatement.add(SqlString.createSqlStringFromString("END;", Category.INSERTED));
+		sqlBlockStatement.add(SqlString.createSqlStringFromString(	"    );"							,Category.INSERTED));
+		sqlBlockStatement.add(SqlString.createSqlStringFromString(	"END;"								,Category.INSERTED));
 
 		SqlCharacter sqlCharacter=tokenStatement.getFirstCharacter();
 		sqlBlockStatement.setBackgroundColor(sqlCharacter.getBackgroundColor());
