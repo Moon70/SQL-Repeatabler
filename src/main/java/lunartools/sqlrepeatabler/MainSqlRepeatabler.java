bcad9abee38a84e36661ae05cb9de8a1dc496d76
Moon70 <52245419+Moon70@users.noreply.github.com>
2025-11-23 08:18:53 +0100

    added recommended licence header to main class.

--- src/main/java/lunartools/sqlrepeatabler/MainSqlRepeatabler.java
+++ src/main/java/lunartools/sqlrepeatabler/MainSqlRepeatabler.java
@@ -12,6 +12,22 @@ import ch.qos.logback.classic.filter.ThresholdFilter;
 import lunartools.sqlrepeatabler.common.SwingBufferingLogBackAppender;
 import lunartools.sqlrepeatabler.gui.SqlRepeatablerView;
 
+/*
+ * Copyright (c) 2025 Thomas Mattel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 public class MainSqlRepeatabler {
 	private static Logger logger = LoggerFactory.getLogger(MainSqlRepeatabler.class);
 
