ef349470c46bcf5c73bc361bfc4f16c5c8433597
Moon70 <52245419+Moon70@users.noreply.github.com>
2025-11-24 13:55:29 +0100

    If input file encoding is not UTF-8, try ISO-8859-15.

--- src/main/java/lunartools/sqlrepeatabler/services/ConverterService.java
+++ src/main/java/lunartools/sqlrepeatabler/services/ConverterService.java
@@ -2,9 +2,15 @@ package lunartools.sqlrepeatabler.services;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.nio.charset.MalformedInputException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lunartools.sqlrepeatabler.SqlRepeatablerModel;
 import lunartools.sqlrepeatabler.parser.SqlBlock;
@@ -12,17 +18,27 @@ import lunartools.sqlrepeatabler.parser.SqlParser;
 import lunartools.sqlrepeatabler.parser.SqlScript;
 
 public class ConverterService {
+    private static Logger logger = LoggerFactory.getLogger(ConverterService.class);
 
-	public ConverterService(SqlRepeatablerModel model) {}
+    public ConverterService(SqlRepeatablerModel model) {}
 
-	public SqlScript createSqlScript(File file) throws Exception{
-		try(BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))){
-			return SqlScript.createInstance(bufferedReader);
-		}
-	}
+    public SqlScript createSqlScript(File file) throws Exception{
+        Path path=file.toPath();
+        String s;
+        try {
+            s=Files.readString(path, StandardCharsets.UTF_8);
+        } catch (MalformedInputException e) {
+            logger.warn("Input file charset is not UTF-8, trying to read as ISO-8859-15. Please check the converted script manually.");
+            s=Files.readString(path, Charset.forName("ISO-8859-15"));
+        }
 
-	public SqlBlock parseFile(SqlScript sqlScript) throws Exception{
-		return SqlParser.parse(sqlScript);
-	}
+        try(BufferedReader bufferedReader=new BufferedReader(new StringReader(s))){
+            return SqlScript.createInstance(bufferedReader);
+        }
+    }
+
+    public SqlBlock parseFile(SqlScript sqlScript) throws Exception{
+        return SqlParser.parse(sqlScript);
+    }
 
 }
